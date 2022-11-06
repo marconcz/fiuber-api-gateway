ffc5dbe8e7d6af6358cf7b21fd3bea0acb576cd1
marconcz <marconcz@gmail.com>
2022-11-06 17:12:42 -0300

    Pushed template

---
+++ api/application/initializer.py
@@ -0,0 +1,18 @@
+class LoggerInstance(object):
+    def __new__(cls):
+        from application.main.utility.logger.custom_logging import LogHandler
+        return LogHandler()
+
+
+class IncludeAPIRouter(object):
+    def __new__(cls):
+        from application.main.routers.hello_world import router as router_hello_world
+        from fastapi.routing import APIRouter
+        router = APIRouter()
+        router.include_router(router_hello_world, prefix='/api/v1', tags=['hello_world'])
+        return router
+
+
+
+# instance creation
+logger_instance = LoggerInstance()
