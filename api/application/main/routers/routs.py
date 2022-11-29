cdbbeccbc763fdb11c7eb62b93da8591d9ceb98f
marconcz <marconcz@gmail.com>
2022-11-29 20:58:01 -0300

    added wallet creation in register

--- api/application/main/routers/routs.py
+++ api/application/main/routers/routs.py
@@ -1,6 +1,5 @@
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
-import requests
 from application.initializer import LoggerInstance
 # Local imports
 from ..services.trips_endpoints import *
@@ -22,9 +21,10 @@ async def price_calculator(distance: float):
 
 #When a Client accept the travel´s price, we save the travel ID
 #in a postgreSQL database waiting for a driver
+
 @router.post("/accept-client-trip")
-async def accept_client_trip(client_id: str, price: float):
-    r = trips_register_trip('accept-client-trip', client_id, price)
+async def accept_client_trip(client_id: str, price: float,user_lat: float,user_long: float,dest_lat: float, dest_long: float):
+    r = trips_register_trip('accept-client-trip', client_id, price, user_lat,user_long,dest_lat, dest_long)
     return r
 
 #If a driver is looking for doing a trip
@@ -35,8 +35,8 @@ async def search_trip():
 
 #If a driver is looking for accept a trip
 @router.post("/accept-driver-trip")
-async def accept_driver_trip(trip_id, driver_id: str):
-    r = trips_register_driver('accept-driver-trip', trip_id, driver_id)
+async def accept_driver_trip(trip_id, driver_id: str, driver_lat: float, driver_long: float):
+    r = trips_register_driver('accept-driver-trip', trip_id, driver_id, driver_lat, driver_long)
     return r
 
 #If a client answer about a driver was found
@@ -44,3 +44,38 @@ async def accept_driver_trip(trip_id, driver_id: str):
 async def check_driver_status(trip_id):
     r = trips_get_driver('driver', trip_id)
     return r
+
+# A driver needs to update his position
+@router.put("/driver/position")
+async def update_position(trip_id, driver_lat: float, driver_long: float):
+    r = update_pos('driver/position', trip_id, driver_lat, driver_long)
+    return r
+
+# A client looking for driver position
+@router.get("/trip-driver-position")
+async def get_driver_position(trip_id):
+    r = get_driver_pos('trip-driver-position', trip_id)
+    return r
+
+# A client want trip status = "Running" or "waiting" (for driver)
+@router.get("/trip")
+async def get_trip(trip_id):
+    return get_trip_status('trip', trip_id)
+
+# Init a Trip by setting status on "Running" from driver device
+@router.put("/init")
+async def init(trip_id):
+    r = init_trip('init', trip_id)
+    return r
+
+@router.get("/passenger/email")
+async def validate_register(email):
+    return validate_user_registered('passenger/email', email)
+
+@router.post("/passengers")
+async def register_passenger(passenger: Passenger):
+    return register_a_passenger('passengers', passenger)
+
+@router.put("/passengers")
+async def update_passenger(passenger: PassengerUpdateSchema):
+    return update_a_passenger('passengers', passenger)
--- api/application/main/services/trips_endpoints.py
+++ api/application/main/services/trips_endpoints.py
@@ -1,7 +1,24 @@
 import requests
+import json
+from pydantic import BaseModel
 from application.initializer import LoggerInstance
-#URL = "https://trips-fiuber.herokuapp.com/"
-URL = "http://localhost:7777/"
+URL = "https://trips-fiuber.herokuapp.com/"
+#URL = "http://localhost:7777/"
+
+class Passenger(BaseModel):
+    email: str
+    password: str
+    passwordConfirmation: str
+    name: str
+    lastname: str
+    birthday: str
+    rol: str
+
+class PassengerUpdateSchema(BaseModel):
+    id: str
+    rol: str
+    name: str
+    lastname: str
 
 logger = LoggerInstance().get_logger(__name__)
 
@@ -18,21 +35,73 @@ def trips_post(url, parameters):
     logger.info('Request URL: {0} , status code = {1}'.format(r.url, r.status_code))
     return r.json()
 
+def trips_put(url, parameters):
+    r = requests.put(url, params = parameters)
+    logger.info('Request URL: {0} , status code = {1}'.format(r.url, r.status_code))
+    return r.json()
+
 def trips_trip_price(endpoint, parameters):
     PARAMS = {'distance' : parameters}
     return trips_get(URL + endpoint, PARAMS)
 
-def trips_register_trip(endpoint, client, price):
-    PARAMS = {'client_id' : client, 'price' : price}
+def trips_register_trip(endpoint, client, price, user_lat,user_long,dest_lat, dest_long):
+    PARAMS = {'client_id' : client,\
+                'price' : price,\
+                'user_lat' : user_lat,\
+                'user_long': user_long,\
+                'dest_lat' : dest_lat,\
+                'dest_long' : dest_long }
     return trips_post(URL + endpoint, PARAMS)
 
 def trips_search_trip(endpoint):
     return trips_get(URL + endpoint, None)
 
-def trips_register_driver(endpoint, trip, driver):
-    PARAMS = {'trip_id' : trip, 'driver_id' : driver}
+def trips_register_driver(endpoint, trip, driver, driver_lat, driver_long):
+    PARAMS = {'trip_id' : trip,\
+                'driver_id' : driver,\
+                'driver_lat' : driver_lat,\
+                'driver_long' : driver_long }
     return trips_post(URL + endpoint, PARAMS)
 
 def trips_get_driver(endpoint, trip):
     PARAMS = {'trip_id' : trip}
     return trips_post(URL + endpoint, PARAMS)
+
+def update_pos(endpoint, trip_id, driver_lat, driver_long):
+    PARAMS = {'trip_id' : trip_id,\
+                'driver_lat' : driver_lat,\
+                'driver_long' : driver_long }
+    return trips_put(URL + endpoint, PARAMS)
+
+def get_driver_pos(endpoint, trip_id):
+   PARAMS = {'trip_id' : trip_id}
+   return trips_get(URL + endpoint, PARAMS)
+
+def get_trip_status(endpoint, trip_id):
+    PARAMS = {'trip_id' : trip_id}
+    return trips_get(URL + endpoint, PARAMS)
+
+def init_trip(endpoint, trip_id):
+    PARAMS = {'trip_id' : trip_id}
+    return trips_put(URL + endpoint, PARAMS)
+
+URL_USERS = "https://test-app-fiuber.herokuapp.com/api/v1/users/"
+
+def validate_user_registered(endpoint, email):
+    PARAMS = {'email' : email}
+    r = requests.post(URL_USERS + endpoint, data = PARAMS)
+    return r.json()
+
+def register_a_passenger(endpoint, passenger: Passenger):
+    wallet = requests.post('http://localhost:3000/wallet')
+    passenger_with_wallet = passenger.dict()
+    passenger_with_wallet['waddress'] = wallet.json()["address"]
+    passenger_with_wallet['key'] = wallet.json()["privateKey"]
+    print(passenger_with_wallet)
+    r = requests.post(URL_USERS + endpoint, data = passenger_with_wallet)
+    return r.json()
+
+def update_a_passenger(endpoint, passenger: PassengerUpdateSchema):
+    print("pasajero:", passenger.dict())
+    r = requests.put(URL_USERS + endpoint, data = passenger.dict())
+    return r.json()
